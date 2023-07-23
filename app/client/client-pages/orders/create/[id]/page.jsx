a41037d8e6c839f84da207e60d41542742d13ed4
bbiabooks <131581766+bbiabooks@users.noreply.github.com>
2023-07-23 09:13:33 +0800

    order fixed

--- app/client/client-pages/orders/create/[id]/page.jsx
+++ app/client/client-pages/orders/create/[id]/page.jsx
@@ -113,10 +113,9 @@ const CreateOrderDetailsPage = ({ params }) => {
             const response = await fetch(`${URL}/api/orders`, {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json',
                     Authorization: `Bearer ${token}`,
                 },
-                body: JSON.stringify(orderData),
+                body: formData,
             });
 
             const data = await response.json();
