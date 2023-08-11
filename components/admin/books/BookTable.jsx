2612f294619c21d04fc002b12eeeda6a0b0c311e
bbiabooks <131581766+bbiabooks@users.noreply.github.com>
2023-08-12 06:44:15 +0800

    md:max

--- components/admin/books/BookTable.jsx
+++ components/admin/books/BookTable.jsx
@@ -132,7 +132,7 @@ const BookTable = ({
                         </div>
                     </div>
                 </div>
-                <div className="overflow-y-auto max-h-[calc(100vh-25vh)] w-full max-w-[928px]">
+                <div className="overflow-y-auto max-h-[calc(100vh-25vh)] w-full md:max-w-[928px]">
                     <table className="table-auto rounded-lg overflow-hidden shadow-lg w-full text-sm">
                         <thead>
                             <tr className="bg-cyan-900">
