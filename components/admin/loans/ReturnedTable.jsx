b9823ea98262dda9966577c4f0f9ef01a9989aac
bbiabooks <131581766+bbiabooks@users.noreply.github.com>
2023-08-12 07:33:22 +0800

    try

--- components/admin/loans/ReturnedTable.jsx
+++ components/admin/loans/ReturnedTable.jsx
@@ -55,7 +55,7 @@ const ReturnedTable = ({
     }
 
     return (
-        <div className="flex-col flex items-start justify-start min-h-full p-12">
+        <div className="flex-col flex items-start justify-start min-h-full max-w-full p-12">
             {/* Add a print-only class to the root div for print styling */ }
             <div className={ `print-only flex-grow ${isPrinting ? "print-table" : ""}` }>
                 <h1 className="text-2xl font-bold mb-6 flex justify-start">
@@ -155,7 +155,7 @@ const ReturnedTable = ({
                         </div>
                     </div>
                 </div>
-                <div className="overflow-y-auto max-h-[calc(100vh-25vh)] w-full md:max-w-[928px]">
+                <div className="overflow-y-auto max-h-[calc(100vh-25vh)] w-full md:w-[928px]">
                     <table className="table-auto rounded-lg overflow-hidden shadow-lg w-full text-sm">
                         <thead>
                             <tr className="bg-cyan-900">
